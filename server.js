58cc80e44596f4b6127f3f0085192c23f1625c9c
qb1968 <qb1968@aol.com>
2021-08-20 11:24:05 -0400

    new

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
